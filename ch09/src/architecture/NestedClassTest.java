83322a472ba85904a0dd8202854e8ffbf9607cc3
BAEKJungHo <designjava@naver.com>
2019-03-24 14:59:48 +0900

    modify

--- ch09/src/architecture/NestedClassTest.java
+++ ch09/src/architecture/NestedClassTest.java
@@ -3,8 +3,8 @@ package architecture;
 public class NestedClassTest {
 	public static void main(String[] args) {
 		OutterClass outter = new OutterClass();
-		OutterClass.InstanceMemberClass iClass = outter.new InstanceMemberClass();
 		
+		OutterClass.InstanceMemberClass iClass = outter.new InstanceMemberClass();
 		OutterClass.StaticMemberClass sClass = new OutterClass.StaticMemberClass();
 
 		outter.method1(8, "john");
