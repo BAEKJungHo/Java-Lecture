b6f61d0f584a3914af2ba997c012666e11a923fc
BAEKJungHo <designjava@naver.com>
2019-03-24 13:44:06 +0900

    Chapter 09

---
+++ ch09/src/architecture/NestedClassTest.java
@@ -0,0 +1,13 @@
+package architecture;
+
+public class NestedClassTest {
+	public static void main(String[] args) {
+		OutterClass outter = new OutterClass();
+		OutterClass.InstanceMemberClass iClass = outter.new InstanceMemberClass();
+		
+		OutterClass.StaticMemberClass sClass = new OutterClass.StaticMemberClass();
+
+		outter.method1(8, "john");
+		iClass.instanceMethod2();
+	}
+}
