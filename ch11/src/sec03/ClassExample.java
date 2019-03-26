89b24de6fb7d28f7dae78655fd9931c7fee62ba3
BAEKJungHo <designjava@naver.com>
2019-03-26 14:40:26 +0900

    add file

---
+++ ch11/src/sec03/ClassExample.java
@@ -0,0 +1,24 @@
+package sec03;
+
+public class ClassExample {
+	public static void main(String[] args) {
+		Car car = new Car();
+		Class clazz1 = car.getClass();
+		System.out.println(clazz1.getName());
+		System.out.println(clazz1.getSimpleName());
+		System.out.println(clazz1.getPackage().getName());
+		System.out.println();
+		
+		try {
+			Class clazz2 = Class.forName("sec03.Car");
+			System.out.println(clazz2.getName());
+			System.out.println(clazz2.getSimpleName());
+			System.out.println(clazz2.getPackage().getName());
+			System.out.println();
+		} catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
