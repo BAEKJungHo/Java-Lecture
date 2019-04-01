146750c02f34b563ed51d88b43b9703e183608a8
BAEKJungHo <designjava@naver.com>
2019-04-01 14:22:38 +0900

    addfile

--- ch11/src/newOpenChallenge/DashInsertExample.java
+++ ch11/src/newOpenChallenge/DashInsertExample.java
@@ -16,13 +16,11 @@ public class DashInsertExample {
 		StringBuilder sb = new StringBuilder();
 		sb.append(str);
 		for(int i=0; i<sb.length()-1; i++) {
-			if(((int)str.charAt(i) % 2 == 0) && ((int)str.charAt(i+1) % 2 == 0) && ((int)str.charAt(i+1) != 42) && ((int)str.charAt(i) != 42)) {
+			if(((int)sb.charAt(i) % 2 == 0) && ((int)sb.charAt(i+1) % 2 == 0) && ((int)sb.charAt(i+1) != 42) && ((int)sb.charAt(i) != 42)) {
 				sb.insert(i+1, "*");
-				str = sb.toString();
 			}
-			else if(((int)str.charAt(i) % 2 != 0) && ((int)str.charAt(i+1) % 2 != 0) && ((int)str.charAt(i+1) != 45) && ((int)str.charAt(i) != 45)) {
+			else if(((int)sb.charAt(i) % 2 != 0) && ((int)sb.charAt(i+1) % 2 != 0) && ((int)sb.charAt(i+1) != 45) && ((int)sb.charAt(i) != 45)) {
 				sb.insert(i+1, "-");
-				str = sb.toString();
 			}
 		}
 		
