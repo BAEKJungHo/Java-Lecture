819284a07d15661386374ea70906567595f702f7
BAEKJungHo <designjava@naver.com>
2019-04-05 13:30:45 +0900

    add file

---
+++ ch12/src/sec04/ThreadStateExample.java
@@ -0,0 +1,10 @@
+package sec04;
+
+public class ThreadStateExample {
+	public static void main(String[] args) {
+		
+		StatePrintThread statePrintThread = new StatePrintThread(new TargetThread());
+		statePrintThread.start();
+
+	}
+}
