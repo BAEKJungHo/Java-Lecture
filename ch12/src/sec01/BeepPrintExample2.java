1263a7d80681f9e16eaaa91335cd1b8f173d09b9
BAEKJungHo <designjava@naver.com>
2019-04-05 10:37:23 +0900

    Chapter 12

---
+++ ch12/src/sec01/BeepPrintExample2.java
@@ -0,0 +1,34 @@
+package sec01;
+
+import java.awt.Toolkit;
+
+public class BeepPrintExample2 {
+	public static void main(String[] args) {
+		/*
+		Runnable beepTask = new BeepTask();
+		Thread thread = new Thread(beepTask);
+		thread.start();
+		
+		for(int i=0; i<5; i++) {
+			System.out.println("띵");
+			try { Thread.sleep(500); } catch(Exception e) { }
+		}
+		*/
+		
+		Thread thread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				Toolkit toolkit = Toolkit.getDefaultToolkit();
+				for(int i=0; i<5; i++) {
+					toolkit.beep();
+					try { Thread.sleep(500); } catch(Exception e) { }
+				}
+			}
+		});
+		
+		for(int i=0; i<5; i++) {
+			System.out.println("띵");
+			try { Thread.sleep(500); } catch(Exception e) { }
+		}
+	}
+}
