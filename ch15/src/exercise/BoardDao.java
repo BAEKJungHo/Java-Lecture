980cd81dd72692b3b90b5a556311585f84b3e7fc
BAEKJungHo <designjava@naver.com>
2019-04-11 08:37:12 +0900

    modify

--- ch15/src/exercise/BoardDao.java
+++ ch15/src/exercise/BoardDao.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BoardDao {
-
 	public static List<Board> getBoardList() {
 		Board board1 = new Board("제목1", "내용1");
 		Board board2 = new Board("제목2", "내용2");
